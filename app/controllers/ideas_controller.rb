dc5f36fdbe8468df44ed97a9da3a760a40d07897
Wendy Ng <yng@ucalgary.ca>
2022-08-02 18:00:53 +0000

    Add like controller, model and routes

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -33,6 +33,7 @@ class IdeasController < ApplicationController
     def show
         @reviews = @idea.reviews.order(created_at: :desc)
         @review = Review.new
+        @like = @idea.likes.find_by(user: current_user)
     end
     # ===============UPDATE==========================
     def edit
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   resources :ideas do
     resources :reviews, only: [:create, :destroy]
+    resources :likes, shallow: true, only: [:create, :destroy]
   end
   resources :users, only:[:new, :create]
 
